69ac7647e4cff7652f036c727d1ce5e8640d5076
hsl18366110807 <18366110807@163.com>
2019-08-06 16:09:43 +0800

    文件夹名字应为英文

---
+++ container/vector/readme.txt
@@ -0,0 +1,5 @@
+![image]
+手动实现vector类
+实现了vector的初始化，insert(),push_back(),size()
+capacity(),以及front()等函数。
+退出函数时，实现对类的析构，对申请空间的释放。
---
+++ container/vector/vectorr.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include"rand.h"
+#include"vectorr.h"
+using namespace std;
+int main() {
+	vectorr<int> vec(3,0);
+	cout << "手动实现vector，类vectorr" << endl;
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec[i] << endl;
+	}
+	cout << "size:" << vec.capacity();
+	cout<<"capacity:"<<vec.capacity();
+	cout << "empty:" << vec.empty();
+	cout << "   " << endl;
+	vec.push_back(1);
+	vec.push_back(2);
+	vec.push_back(3);
+	vec.push_back(4);
+	vec.erase(vec.begin());
+	vec.insert(vec.begin(), 0);
+	vec.insert(vec.begin(), 5);
+	vec.insert(vec.begin(), 6);
+	for (int i = 0; i < vec.size(); i++) {
+		cout << vec[i] << endl;
+	}
+	cout << "back:" << "  " << vec.back() << endl;
+	cout << "front:" << "  " << vec.front() << endl;
+	cout << "size:" << vec.size();
+	cout << "capacity:" << vec.capacity();
+	cout << "empty:" << vec.empty();
+	system("pause");
+}
