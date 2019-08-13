1c08f3f942ddcb9d8efd62d6f97b3b607416a160
subaolong <suaspire@163.com>
2019-08-13 17:29:53 +0800

    附上allocator底层实现的函数，申请是new释放是delete

--- container/vector/vectorr.h
+++ container/vector/vectorr.h
@@ -36,7 +36,7 @@ public:
 		}
 		else {
 			size_type n = capacity();
-			pointer iter = reinterpret_cast<T*>(malloc(sizeof(value_type) * n * 2));
+			pointer iter = reinterpret_cast<pointer>(allocat.allocate(sizeof(value_type) * n * 2));
 			pointer s = iter;
 			for (iterator it = start; it < finish; it++) {
 				*iter++ = *it;
@@ -115,3 +115,71 @@ public:
 	reference operator[](size_type n) { return *(begin() + n); }
 };
 
+//template <typename T>
+//inline T* _allocate(ptrdiff_t size, T*) {
+//	set_new_handler(0);
+//	T* tmp = (T*)(::operator new((size_t)(size * sizeof(T))));
+//	if (!tmp) {
+//		cerr << "out of memory" << endl;
+//		exit(1);
+//	}
+//	return tmp;
+//}
+//
+//template <typename T>
+//inline void _deallocate(T* buffer) {
+//	::operator delete(buffer);
+//}
+//
+//template <typename T1, typename T2>
+//inline void _construct(T1* p, const T2& value) {
+//	new(p) T1(value);           // placement new ²Î¿¼: cpp primer.
+//}
+//
+//template <typename T>
+//inline void _destroy(T* ptr) {
+//	ptr->~T();
+//}
+//
+//template <typename T>
+//class allocator {
+//public:
+//	typedef T           value_type;
+//	typedef T*          pointer;
+//	typedef const T*    const_pointer;
+//	typedef T&          reference;
+//	typedef const T&    const_reference;
+//	typedef size_t      size_type;
+//	typedef ptrdiff_t   difference_type;
+//
+//	template <typename U>
+//	struct rebind {
+//		typedef allocator<U> other;
+//	};
+//
+//	pointer allocate(size_type n, const void* hint = 0) {
+//		pv("allocate %d for type %s\n", n, typeid(T).name());
+//		return _allocate((difference_type)n, (pointer)0);
+//	}
+//
+//	void deallocate(pointer p, size_type n) {
+//		pv("deallocate %d for type %s\n", n, typeid(*p).name());
+//		_deallocate(p);
+//	}
+//
+//	void destroy(pointer p) {
+//		_destroy(p);
+//	}
+//
+//	pointer address(reference x) {
+//		return (pointer)&x;
+//	}
+//
+//	const_pointer address(const_reference x) {
+//		return (const_pointer)&x;
+//	}
+//
+//	size_type max_size() const {
+//		return size_type(UINTMAX_MAX / sizeof(T));
+//	}
+//};
