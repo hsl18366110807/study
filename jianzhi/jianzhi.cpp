b0a39b779cbcc8ab7da2a5c122219cc41ab8d302
HaoShengli <18366110807@163.com>
2022-03-21 18:10:11 +0800

    丑数和二叉搜索树的判断

---
+++ jianzhi/jianzhi.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int cuttingRope(int n){
+     if(n==1) return 1;
+     if(n==2) return 1;
+     if(n==3) return 2;
+     vector<int> vec(n + 1, 0);
+     int index = 1;
+     vec[index++]=1;
+     vec[index++]=2;
+     vec[index++]=3;
+     while(index<=n){
+         int res = 0;
+         for(int i = 1;i<=index/2;++i){
+             int tmp = vec[i]*vec[index - i];
+             if(tmp>res)res = tmp;
+         }
+         vec[index++] = res;
+     }
+     return vec[n];
+}
+bool jianyan(TreeNode* root, int val, bool flag){ //flag = true 左孩子
+        if(root == nullptr) return true;
+        if(flag){
+            if(root->val>=val) return false;
+            return jianyan(root->left, val, true) && jianyan(root->right, val, true);
+        }else{
+            if(root->val<=val) return false;
+            return jianyan(root->left, val, false) && jianyan(root->right, val, false);
+        }
+}
+
+bool isValidBST(TreeNode* root) {
+        if(root == nullptr) return true;
+        if(!jianyan(root->left, root->val, true)) return false;
+        if(!jianyan(root->right, root->val, false)) return false;
+        return isValidBST(root->left) && isValidBST(root->right);
+}
+int nthUglyNumber(int n) {
+    vector<int> dp(n, 0);
+    dp[0] = 1;
+    int index = 1;
+    int a = 0, b = 0, c = 0;
+    while(index<=n-1){
+        int tmp = min(min(dp[a]*2, dp[b]*3),dp[c]*5);
+        if(tmp == dp[a]*2) ++a;
+        if(tmp == dp[b]*3) ++b;
+        if(tmp == dp[c]*5) ++c;
+        dp[index++] = tmp;
+    }
+    return dp[n-1];
+}
+int main(){
+	
+}
+
