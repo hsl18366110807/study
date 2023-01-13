aa36e4ec9a5f6a0fe9fe01fb92e369fe2b4a20c9
haoshengli <18366110807@163.com>
2023-01-13 15:01:49 +0800

    工具2.0

--- acculate_line/mainwindow.cpp
+++ acculate_line/mainwindow.cpp
@@ -1,30 +1,31 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include <qdir.h>
+#include <qset.h>
 #include <QFileDialog>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow), m_target_file_num(0), m_target_source_num(0)
-    , m_protect_count(100000), m_auto_filter_type(true)
+    , m_protect_count(100000), m_dir_path("")
 {
     ui->setupUi(this);
-    init_data();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
-}
-
-void MainWindow::init_data(){
-    ui->m_combox_file_type->addItem("cpp");
-    ui->m_combox_file_type->addItem("h");
-    m_filter_type = ui->m_combox_file_type->currentText();
+    m_target_file_num = 0;
+    m_target_source_num = 0;
+    m_protect_count = 0;
+    m_filter_type = "";
+    m_dir_path = "";
+    m_file_type_set.clear();
 }
 
 void MainWindow::analyse_dir(QString dir_path){
     QDir Dir(dir_path);
-    if(!Dir.exists()||m_filter_type == "") return;//查看工作路径是否存在
+    if(!Dir.exists()) return;//查看工作路径是否存在
 
     QStringList dir_list = Dir.entryList(QDir::Dirs);
     //递归查询文件夹
@@ -32,22 +33,18 @@ void MainWindow::analyse_dir(QString dir_path){
         QString tmp_dir_path = Dir.filePath(dir_list[i]);
         analyse_dir(tmp_dir_path);
     }
-
     QStringList list = Dir.entryList(QDir::Files);  			//获取所有文件
     foreach (QFileInfo file, list)
     {
-        if(file.fileName().split(".").back() == m_filter_type)          //判断进行再次确认是不是目标文件
-        {
-            QString tmp_file_path = Dir.filePath(file.fileName());
-            analyse_file(tmp_file_path);
-            ++m_target_file_num;
-        }
+        QString file_type = file.fileName().split(".").back();
+        if(!m_file_type_set.contains(file_type)) m_file_type_set.insert(file_type);
     }
 }
 
 void MainWindow::analyse_file(QString file_path){
     QFile file(file_path);
     if(!file.exists()) return;
+    m_dir_path = file_path;
     if (!file.open(QIODevice::ReadOnly))
         return;
     while (!file.atEnd()) {
@@ -70,24 +67,22 @@ bool MainWindow::isDirExist(QString fullPath){
 }
 
 void MainWindow::begin_analyse(QString path){
-    ui->m_textEdit_files->clear();
-    m_target_file_num = 0;
+    ui->m_combox_file_type->clear();
+    m_file_type_set.clear();
+    m_target_file_num= 0;
     m_target_source_num = 0;
-
     if(isDirExist(path)) analyse_dir(path);
     else{
         analyse_file(path);
         m_target_file_num = 1;
     }
 
+    foreach (QString type, m_file_type_set) {
+        ui->m_combox_file_type->addItem(type);
+    }
+
     ui->m_lineEdit_files_count->setText(QString::number(m_target_file_num, 10));
     ui->m_lineEdit_source_count->setText(QString::number(m_target_source_num, 10));
-}
-
-void MainWindow::on_m_button_begin_clicked()
-{
-    QString dir_path = ui->m_lineEdit_dir_path->text();
-    this->begin_analyse(dir_path);
 }
 
 void MainWindow::on_m_button_choose_dir_clicked()
@@ -99,24 +94,46 @@ void MainWindow::on_m_button_choose_dir_clicked()
 }
 
 
-void MainWindow::on_m_button_manul_filter_clicked()
-{
-    m_auto_filter_type = !m_auto_filter_type;
-    if(m_auto_filter_type){
-        ui->m_button_manul_filter->setText("auto filter");
-        m_filter_type = ui->m_combox_file_type->currentText();
-    }else{
-      ui->m_button_manul_filter->setText("manul filter");
-      m_filter_type = ui->m_text_input_type->text();
+void MainWindow::filter_files(QString file_type){
+    QDir Dir(m_dir_path);
+    if(!Dir.exists()) return;//查看工作路径是否存在
+    QStringList dir_list = Dir.entryList(QDir::Dirs);
+    //递归查询文件夹
+    for(int i=2;i<dir_list.count();++i){
+        QString tmp_dir_path = Dir.filePath(dir_list[i]);
+        m_dir_path = tmp_dir_path;
+        filter_files(file_type);
+    }
+    //筛选
+    QStringList list = Dir.entryList(QDir::Files);  			//获取所有文件
+    foreach (QFileInfo file, list)
+    {
+        if(file.fileName().split(".").back() == file_type)          //判断进行再次确认是不是目标文件
+        {
+            QString tmp_file_path = Dir.filePath(file.fileName());
+            analyse_file(tmp_file_path);
+            ++m_target_file_num;
+        }
     }
 }
 
-void MainWindow::on_m_combox_file_type_currentIndexChanged(const QString &arg1)
+void MainWindow::on_m_button_analyse_clicked()
 {
-    if(m_auto_filter_type) m_filter_type = arg1;
+    begin_analyse(m_dir_path);
 }
 
-void MainWindow::on_m_text_input_type_textChanged(const QString &arg1)
+void MainWindow::on_m_lineEdit_dir_path_textChanged(const QString &arg1)
 {
-    if(!m_auto_filter_type) m_filter_type = arg1;
+    m_dir_path = arg1;
+}
+
+void MainWindow::on_m_combox_file_type_activated(const QString &arg1)
+{
+    ui->m_textEdit_files->clear();
+    m_target_file_num = 0;
+    m_target_source_num = 0;
+    m_dir_path = ui->m_lineEdit_dir_path->text();
+    filter_files(arg1);
+    ui->m_lineEdit_files_count->setText(QString::number(m_target_file_num, 10));
+    ui->m_lineEdit_source_count->setText(QString::number(m_target_source_num, 10));
 }
--- acculate_line/mainwindow.h
+++ acculate_line/mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <qset.h>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -17,17 +17,19 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_m_button_begin_clicked();
     void on_m_button_choose_dir_clicked();
-    void on_m_button_manul_filter_clicked();
-    void on_m_combox_file_type_currentIndexChanged(const QString &arg1);
-    void on_m_text_input_type_textChanged(const QString &arg1);
+
+    void on_m_button_analyse_clicked();
+
+    void on_m_lineEdit_dir_path_textChanged(const QString &arg1);
+
+    void on_m_combox_file_type_activated(const QString &arg1);
 
 private:
-    void init_data();
     void begin_analyse(QString dir_path);
     void analyse_dir(QString dir_path);
     void analyse_file(QString file_path);
+    void filter_files(QString file_type);
     bool isDirExist(QString fullPath);
 
 private:
@@ -35,7 +37,8 @@ private:
     int m_target_file_num;
     int m_target_source_num;
     int m_protect_count;
-    bool m_auto_filter_type;
     QString m_filter_type;
+    QString m_dir_path;
+    QSet<QString> m_file_type_set;
 };
 #endif // MAINWINDOW_H
