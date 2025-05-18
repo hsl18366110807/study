996154090752c6592425d1f34a5fdf5a67aca55d
HaoShengli <18366110807@163.com>
2025-05-19 00:25:54 +0800

    5-19 动态规划

--- jianzhi/jianzhi.cpp
+++ jianzhi/jianzhi.cpp
@@ -59,6 +59,51 @@ int nthUglyNumber(int n) {
     }
     return dp[n-1];
 }
+//2025-5-18 everyday 爬楼梯
+int climbStairs(int n) {
+    //dp[i] = dp[i-1] + dp[i-2] 状态方程
+    if(n <= 0) return 0;
+    if(n == 1) return 1;
+    if(n == 2) return 2;
+    vector<int> dp(n + 1, 0);
+    dp[1] = 1;
+    dp[2] = 2;
+    for(int i = 3; i <= n; ++i){
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}
+//打家劫舍
+int rob(vector<int>& nums) {
+    if(nums.size() == 0) return 0;
+    if(nums.size() == 1) return nums[0];
+    if(nums.size() == 2) return max(nums[0], nums[1]);
+    vector<int> dp(nums.size(), 0);
+    dp[0] = nums[0];
+    dp[1] = max(nums[0], nums[1]);
+    for(int i = 2; i < nums.size(); ++i){
+        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+    }
+    return dp[nums.size() - 1];
+}
+
+//等差数列划分
+int numberOfArithmeticSlices(vector<int>& nums) {
+    if(nums.size() < 3) return 0;
+    int res = 0;
+    int count = 0;
+    for(int i = 2; i < nums.size(); ++i){
+        if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]){
+            ++count;
+            res += count;
+        }else{
+            count = 0;
+        }
+    }
+    return res;
+    
+}
+
 int main(){
 	
 }
